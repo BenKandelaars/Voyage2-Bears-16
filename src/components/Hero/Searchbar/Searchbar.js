297c34191dc5b2291cab7747baf2557830666991
Lukasz <lukaszandruchow@gmail.com>
2017-09-28 13:45:21 +0100

    3lements of Searchbar still hold their width even when they stick together

--- src/components/Hero/Searchbar/Searchbar.js
+++ src/components/Hero/Searchbar/Searchbar.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
-import { SearchContainer, ButtonWrapper, Button, InputPlusLabel, InputGroup, LOGO, Icon } from './styles';
+import { SearchContainer, ButtonWrapper, Button, InputWrapper, InputPlusLabel, InputGroup, LOGO, Icon } from './styles';
 
 export class Searchbar extends Component {
   render() {
     return (
       <SearchContainer className="Search-Container">
+        <InputWrapper>
+          <InputGroup className="Input-Group">
+            <Icon>
+              <LOGO className="fa fa-search" aria-hidden="true" />
+            </Icon>
+            <InputPlusLabel>
+              <p>Where?</p>
+              <input type="text" placeholder="Anywhere" name="Where"/>
+            </InputPlusLabel>
+          </ InputGroup>   
 
-        <InputGroup className="Input-Group">
-          <Icon>
-            <LOGO className="fa fa-search" aria-hidden="true" />
-          </Icon>
-          <InputPlusLabel>
-            <p>Where?</p>
-            <input type="text" placeholder="Anywhere" name="Where"/>
-          </InputPlusLabel>
-        </ InputGroup>   
+          <InputGroup className="Input-Group">
+            <Icon>
+              <LOGO className="fa fa-calendar-o" aria-hidden="true" />
+            </Icon>
+            <InputPlusLabel>
+              <p>When?</p>
+              <input placeholder="From - To" type="text" name="When" />
+            </InputPlusLabel>
+          </ InputGroup>
 
-        <InputGroup className="Input-Group">
-          <Icon>
-            <LOGO className="fa fa-calendar-o" aria-hidden="true" />
-          </Icon>
-          <InputPlusLabel>
-            <p>When?</p>
-            <input placeholder="From - To" type="text" name="When" />
-          </InputPlusLabel>
-        </ InputGroup>
-
-        <InputGroup className="Input-Group">
-          <Icon>
-            <LOGO className="fa fa-users" aria-hidden="true" />
-          </Icon>
-          <InputPlusLabel>
-            <p>Guests</p>
-            <input placeholder="1 Guest" type="text" name="Guests" />
-          </InputPlusLabel>
-          
-        </ InputGroup>
+          <InputGroup className="Input-Group">
+            <Icon>
+              <LOGO className="fa fa-users" aria-hidden="true" />
+            </Icon>
+            <InputPlusLabel>
+              <p>Guests</p>
+              <input placeholder="1 Guest" type="text" name="Guests" />
+            </InputPlusLabel>
+            
+          </ InputGroup>
+        </ InputWrapper>
         <ButtonWrapper className="Button-Wrapper">
           <Button className="Button">
             Search
--- src/components/Hero/Searchbar/styles.js
+++ src/components/Hero/Searchbar/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const SearchContainer = styled.div`
     display: flex;
-    flex-flow: row no-wrap;
+    flex-flow: row nowrap;
     border: 1px solid grey;
     border-radius: 3px;
     margin: 0.8em;
@@ -30,22 +30,26 @@ export const Button = styled.div`
 
 export const InputGroup = styled.div`
     display: flex;
-    flex-flow: row no-wrap;
+    flex-flow: row nowrap;
     padding: 0.7em;
-    
-    @media(min-width: 800px){
+
+    @media(min-width:750px){
         width: 30%;
-    }
 
-    :nth-last-child(-n+3){
-        border-left: 1px solid grey;
+        :nth-last-child(-n+3){
+            border-left: 1px solid grey;
+        }
+    
     }
-
+    
 `;
 export const Icon = styled.div`
     padding: 0em 0.3em;
     font-size: 1em ;
 
+    @media(max-width:750px){
+        display:none;
+    }
     @media(min-width:1080px){
         display: none;
     }
@@ -54,20 +58,31 @@ export const LOGO = styled.div`
     color: grey;
     display:block;
 
-    
+`;
+
+export const InputWrapper = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+   
+    @media(min-width:750px){
+        width:100%;
+    }
 `;
 
 export const InputPlusLabel = styled.div`
     display: flex;
     flex-flow: column wrap;
-    width: 100%;
     
     input {
+        display: flex;
         border: none;
+        min-width:100px;
+
         :focus{
             outline: none;
             placeholder: none;
         }
+
         @media(max-width:1080px){
             height:100%;
         }
@@ -78,26 +93,14 @@ export const InputPlusLabel = styled.div`
         font-size: 1.35em;
     }
 
-    select {
-        border: none;
-        :focus{
-            outline: none;
-
-        }
-        @media(max-width:1080px){
-            height:100%;
-        }
-    }
-
     p {
         display: none;
         font-size: 1em;
-        @media(min-width:800px) {
-            padding: 0.2em 0em;
-            margin: 0.2em 0em;
-        }
+
         @media(min-width:1080px) {
             display: inline;
+            padding: 0.2em 0em;
+            margin: 0.2em 0em;
         }
     }
 `;
