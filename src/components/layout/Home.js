481caf8f5566ad353f18699a7b5b8c2185ab661c
Lukasz <lukaszandruchow@gmail.com>
2017-09-29 14:28:46 +0100

    successful migration of Home.js, Hero to new folder.

---
+++ src/components/layout/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel';
+import Sitemap from '../sitemap/Sitemap';
+import Hero from '../presentational/Hero/Hero';
+
+const Home = () => (
+  <div>
+    <Hero />
+    <Carousel />
+    <Sitemap />
+  </div>
+);
+
+export default Home;
