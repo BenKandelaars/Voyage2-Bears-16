2cc82ff2372860486d113e3a6f0b7af905a7ddc0
Lukasz <lukaszandruchow@gmail.com>
2017-09-29 13:44:35 +0100

    polishing the Home.js file (class -> func)

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Carousel from '../components/Carousel/Carousel';
 import Sitemap from '../components/sitemap/Sitemap';
 import { Hero } from '../components/Hero/Hero';
 
-class Home extends Component {
-  
-  render() {
-    return (
-      <div>
-        <Hero />
-        <Carousel />
-        <Sitemap />
-      </div>
-    )
-  }
-};
+const Home = () => (
+  <div>
+    <Hero />
+    <Carousel />
+    <Sitemap />
+  </div>
+);
 
-export { Home };
+export default Home;
