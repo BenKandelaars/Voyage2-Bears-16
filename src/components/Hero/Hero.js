06c1a972e9dc349c7d5ef02c0cd638a871e2c77a
Lukasz <lukaszandruchow@gmail.com>
2017-09-29 13:56:13 +0100

    Swapping 'text-al:center' to styles of Carousel (from app.css)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Searchbar } from './Searchbar/Searchbar'
-import { HeroContainer, Header, HeaderContainer } from './styles'
+import { Searchbar } from './Searchbar/Searchbar';
+import { HeroContainer, Header, HeaderContainer } from './styles';
 
-export const Hero = () => (
-      <HeroContainer className="Hero-Container">
-        <HeaderContainer className="Header-Container">
-          <Header className="Hero-header">
-            <h1>Airbnb</h1>
-            <p className="Hero-intro">
+export default function Hero() {
+  return (
+    <HeroContainer className="Hero-Container">
+      <HeaderContainer className="Header-Container">
+        <Header className="Hero-header">
+          <h1>Airbnb</h1>
+          <p className="Hero-intro">
               Book unique homes and experience a city like a local.
-            </p>
-          </Header>
-          <Searchbar className="Searchbar"/>
-        </HeaderContainer>
-      </ HeroContainer>
-    );
+          </p>
+        </Header>
+        <Searchbar className="Searchbar" />
+      </HeaderContainer>
+    </HeroContainer>
+  )
+;
+}
 
--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Carousel from '../components/Carousel/Carousel';
 import Sitemap from '../components/sitemap/Sitemap';
-import { Hero } from '../components/Hero/Hero';
+import Hero from '../components/Hero/Hero';
 
 const Home = () => (
   <div>
