08fd6173d56d3de9b93eed091d6051a0609fe2d0
unknown <chalubinska.paulina@gmail.com>
2017-09-29 12:04:03 +0200

    First attempt create dropdown

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import img from "./koala-headphone-logo.jpg"
 
 class NavComponent extends React.Component {
+
+	showmenu() {
+		let menu = document.querySelector('.Buttons');
+		if (menu.style.display === 'block') {
+			menu.style.display = 'none';
+		} else {
+			menu.style.display = 'block';
+		}
+	}
 	render() {
 		return (
 			<nav>
 				<div className="Banner">
-					<div className="Logo">
+					<div className="Logo" onClick={this.showmenu}>
 						<a href="#"><img src={img} width="100px" height="64px" alt="home" /></a>
 					</div>
 					<div className="Blank">
 					</div>
-					<div className="Buttons">
-						<button className="Buttons--action"  title="Become a Host">
-							<span>Become a Host</span>
+					<div className="Buttons">	
+						<button className="Buttons--action" title="Become a Host" aria-hidden="true">
+							<span>Become a Host</span>	
 						</button>
-						<button className="Buttons--action" title="Help">
+						<button className="Buttons--action" title="Help" aria-hidden="true">
 							<span>Help</span>
 						</button>
-						<button className="Buttons--action" title="Sign up">
+						<button className="Buttons--action" title="Sign up" aria-hidden="true">
 							<span>Sign up</span>
 						</button>
-						<button className="Buttons--action" title="Log in">
+						<button className="Buttons--action" title="Log in" aria-hidden="true">
 							<span>Log in</span>
 						</button>		
-					</div>								
+					</div>	
+					<div className="Overlay">
+					</div>							
 				</div>
 			</nav>
 		);
