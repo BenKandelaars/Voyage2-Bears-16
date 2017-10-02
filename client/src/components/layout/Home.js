42e9e681e1ee2687070091a866f03be0340fc12d
Ben <dutchpossum@hotmail.com>
2017-10-02 16:42:30 +0100

    Complete file restructure, fixing issues

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Carousel from '../presentational/Carousel/Carousel';
-import Sitemap from '../presentational/sitemap/Sitemap';
 import Hero from '../presentational/Hero/Hero';
-
+import Carousel from '../presentational/Carousel/Carousel';
+import Cards from '../presentational/Cards/Cards';
+import Sitemap from '../presentational/Sitemap/Sitemap';
 
 const Home = () => (
   <div>
     <Hero />
     <Carousel />
+    <Cards />
     <Sitemap />
   </div>
 );
