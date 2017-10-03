8605f3b1a77c6a3276dc69677330f5f134fb3126
unknown <chalubinska.paulina@gmail.com>
2017-10-03 14:03:56 +0200

    start

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import Hero from '../presentational/Hero/Hero';
-import Carousel from '../presentational/Carousel/Carousel';
-import Cards from '../presentational/Cards/Cards';
-import Sitemap from '../presentational/Sitemap/Sitemap';
-import NavComponent from '../presentational/Navbar/Navbar';
+import Slider from '../presentational/Slider/Slider';
 
 const Home = () => (
   <div>
-    <NavComponent />
-    <Hero />
-    <Carousel />
-    <Cards />
-    <Sitemap />
+    <Slider />
   </div>
 );
 
---
+++ client/src/components/presentational/Slider/Slider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Slider = () => (
+  <div>
+    <h1>blaaa</h1>
+  </div>
+);
+
+export default Slider;
