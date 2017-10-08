7d7a331c0420f9c78b939927067b8ff78b1d056d
unknown <chalubinska.paulina@gmail.com>
2017-10-08 23:41:03 +0200

    Slider wireframe

--- client/src/components/presentational/Slider/Slider.js
+++ client/src/components/presentational/Slider/Slider.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import '../../assets/font-awesome-4.7.0/css/font-awesome.css';
+// import FontAwesome from 'react-fontawesome';
+import img from './img/image-1.jpg';
 
-const Slider = () => (
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 347,
+      height: 240,
+    };
+  }
+  nextSlide() {
+    console.log('clicked');
+  }
+
+  render() {
+    return (
+      <div className="Wrapper">
+        <div className="SliderView">
+          <div className="SliderArrows">
+            <RightArrow />
+            <LeftArrow />
+          </div>
+          <img src={img} className="Img" alt="" width="347px" height="240px" />
+        </div>
+        <div className="Description" />
+      </div>
+    );
+  }
+}
+
+export default Slider;
+
+const RightArrow = () => (
   <div>
-    <h1>blaaa</h1>
+    <div className="RightArrow" role="button" focusable="true" onClick={this.nextSlide}>
+      <span className="fa fa-angle-right" style={{fontSize: 35 }} aria-hidden="true" />
+    </div>
   </div>
 );
 
-export default Slider;
+
+const LeftArrow = () => (
+  <div>
+    <div className="LeftArrow" role="button" focusable="true" onClick={this.nextSlide}>
+      <span className="fa fa-angle-left" style={{fontSize: 35 }} aria-hidden="true" />
+    </div>
+  </div>
+);
